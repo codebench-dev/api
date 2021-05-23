d48e638c8ac34afe1b03f2fdf5d33992982dfa07
Gwendal <gwendou95@hotmail.fr>
2021-05-23 16:52:39 +0200

    fix: add missing module import

--- src/benchmarks/benchmark.module.ts
+++ src/benchmarks/benchmark.module.ts
@@ -1,13 +1,21 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  forwardRef,
+} from '@nestjs/common';
+import { UsersModule } from 'src/users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Benchmark } from './benchmark.entity';
 import { BenchmarkService } from './benchmark.service';
 import { BenchmarkController } from './benchmark.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Benchmark])],
+  imports: [TypeOrmModule.forFeature([Benchmark]),
+    forwardRef(() => UsersModule),
+  ],
   providers: [BenchmarkService],
   controllers: [BenchmarkController],
   exports: [BenchmarkService],
+
 })
-export class BenchmarkModule {}
+export class BenchmarkModule {
+}
