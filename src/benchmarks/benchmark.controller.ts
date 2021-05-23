97db756479293ff7f6b57cf71b6b02a029e7e8d2
Gwendal <gwendou95@hotmail.fr>
2021-05-23 16:30:56 +0200

    fix: protect benchmark creation route & link benchmark to user

--- src/benchmarks/benchmark.controller.ts
+++ src/benchmarks/benchmark.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { CreateBenchmarkDto } from 'src/benchmarks/dto/create-benchmark.dto';
 import { BenchmarkService } from 'src/benchmarks/benchmark.service';
 import { Benchmark } from './benchmark.entity';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { ValidatedJWTReq } from '../auth/dto/validated-jwt-req';
 
 
 @Controller('benchmarks')
@@ -10,12 +12,14 @@ export class BenchmarkController {
   }
 
   @Post()
+  @UseGuards(JwtAuthGuard)
   async createBenchmark(
+    @Request() req: ValidatedJWTReq,
     @Body()
       benchmark: CreateBenchmarkDto
   ) : Promise<Benchmark>{
     console.log(benchmark);
-    return this.benchmarkService.create(benchmark);
+    return this.benchmarkService.create(benchmark, req.user);
   }
 
 }
--- src/benchmarks/benchmark.service.ts
+++ src/benchmarks/benchmark.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBenchmarkDto } from './dto/create-benchmark.dto';
 import { Benchmark } from './benchmark.entity';
+import { User } from '../users/user.entity';
 
 @Injectable()
 export class BenchmarkService {
 
-  async create(benchmarkData: CreateBenchmarkDto): Promise<Benchmark> {
+  async create(benchmarkData: CreateBenchmarkDto, user: User): Promise<Benchmark> {
     const benchmark = new Benchmark(benchmarkData);
-    // TODO : Link benchmark to user
+    benchmark.creator = user;
     return benchmark.save();
   }
 }
