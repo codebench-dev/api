96f789578c7ee309647cf2bd309cfe038082927a
Stanislas <stanislas.lange@pm.me>
2021-05-31 17:59:28 +0200

    perf: use fastify instead of express (#31)
    https://docs.nestjs.com/techniques/performance

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,22 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
   app.enableCors();
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
