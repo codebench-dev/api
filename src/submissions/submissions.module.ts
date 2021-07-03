1e957be00d63411f573435826782cd7eb27041bf
Stanislas Lange <stanislas.lange@pm.me>
2021-07-03 14:07:59 +0000

    fix(rabbitmq): get RABBITMQ_URL from ConfigService

--- src/submissions/submissions.module.ts
+++ src/submissions/submissions.module.ts
@@ -1,5 +1,6 @@
 import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 import { CacheModule, forwardRef, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from 'src/users/users.module';
 import { BenchmarkModule } from '../benchmarks/benchmark.module';
@@ -12,14 +13,20 @@ import { SubmissionsService } from './submissions.service';
     TypeOrmModule.forFeature([Submission]),
     forwardRef(() => UsersModule),
     CacheModule.register(),
-    RabbitMQModule.forRoot(RabbitMQModule, {
-      exchanges: [
-        {
-          name: 'jobs_ex',
-          type: 'direct',
-        },
-      ],
-      uri: process.env.RABBITMQ_URL || 'amqp://admin:admin@localhost:5672/',
+    RabbitMQModule.forRootAsync(RabbitMQModule, {
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        exchanges: [
+          {
+            name: 'jobs_ex',
+            type: 'direct',
+          },
+        ],
+        uri:
+          configService.get<string>('RABBITMQ_URL') ||
+          'amqp://admin:admin@localhost:5672/',
+      }),
     }),
     forwardRef(() => BenchmarkModule),
   ],
