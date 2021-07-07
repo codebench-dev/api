01972ee4b3ea01f9ae2fa7f68bf8934d4bee1675
Gwendal <gwendou95@hotmail.fr>
2021-07-08 00:13:42 +0200

    feat: add go quality rules

--- src/code-quality/code-quality.service.ts
+++ src/code-quality/code-quality.service.ts
@@ -2,12 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { QualityDTO } from './dto/quality.dto';
 import { CPPQualityVisitor } from './languages/cpp/cpp.vistor';
 import { PythonVisitor } from './languages/python/python.visitor';
+import { GoQualityVisitor } from './languages/golang/go.visitor';
 
 @Injectable()
 export class CodeQualityService {
   public run(source: string, language: string): QualityDTO {
     const cppVisitor = new CPPQualityVisitor();
     const pyVisitor = new PythonVisitor();
+    const goVisitor = new GoQualityVisitor();
+
     const ret = {
       score: 100,
     };
@@ -17,6 +20,8 @@ export class CodeQualityService {
         return cppVisitor.run(source);
       case 'py':
         return pyVisitor.run(source);
+      case 'go':
+        return goVisitor.run(source);
       default:
         return ret;
     }
