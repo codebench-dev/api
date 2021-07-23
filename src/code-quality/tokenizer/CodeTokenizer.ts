c4b26f23c13fd8ae174e9d86bb28dd2898afc9f4
Gwendal <gwendou95@hotmail.fr>
2021-07-24 01:25:49 +0200

    feat: implement golang and fix empty lines

--- src/code-quality/tokenizer/CodeTokenizer.ts
+++ src/code-quality/tokenizer/CodeTokenizer.ts
@@ -1,20 +1,25 @@
 import { CodeTokens } from './CodeTokens';
 import { PythonIdentifiers } from './identifiers/PythonIdentifiers';
 import { CommonLangIdentifiers } from './identifiers/CommonLangIdentifiers';
+import { GolangIdentifiers } from './identifiers/GolangIdentifiers';
 
 export class CodeTokenizer {
   private pyIdentifiers = new PythonIdentifiers();
 
-  public tokenizeCode(code: string, language: string): CodeTokens[] {
+  private golangIdentifiers = new GolangIdentifiers();
+
+  public tokenize(code: string, language: string): CodeTokens[] {
     switch (language) {
       case 'python':
-        return this.tokenizePython(code, this.pyIdentifiers);
+        return this.tokenizeCode(code, this.pyIdentifiers);
+      case 'go':
+        return this.tokenizeCode(code, this.golangIdentifiers);
       default:
         return [];
     }
   }
 
-  private tokenizePython(
+  private tokenizeCode(
     code: string,
     languageIdentifier: CommonLangIdentifiers,
   ): CodeTokens[] {
@@ -24,68 +29,71 @@ export class CodeTokenizer {
     lines.forEach((line) => {
       const trimedLine = line.trim();
 
-      // Function detection
-      if (
-        trimedLine.startsWith(languageIdentifier.functionIdentifier()) &&
-        trimedLine.endsWith(languageIdentifier.endLoopAndCondIdentifier())
-      ) {
-        tokenizedCode.push(CodeTokens.FUNC);
-        const numberOfParams =
-          CodeTokenizer.numberOfFunctionParameters(trimedLine);
-        tokenizedCode = CodeTokenizer.pushTokensInTab(
-          tokenizedCode,
-          CodeTokens.FUNCPARAM,
-          numberOfParams,
-        );
+      if (trimedLine !== '' && trimedLine !== '}')
+        if (
+          trimedLine.startsWith(languageIdentifier.functionIdentifier()) &&
+          trimedLine.endsWith(languageIdentifier.endLoopAndCondIdentifier())
+        ) {
+          // Function detection
+          tokenizedCode.push(CodeTokens.FUNC);
+          const numberOfParams =
+            CodeTokenizer.numberOfFunctionParameters(trimedLine);
+          tokenizedCode = CodeTokenizer.pushTokensInTab(
+            tokenizedCode,
+            CodeTokens.FUNCPARAM,
+            numberOfParams,
+          );
 
-        // For line
-      } else if (
-        trimedLine.startsWith(languageIdentifier.forIdentifier()) &&
-        trimedLine.endsWith(languageIdentifier.endLoopAndCondIdentifier())
-      ) {
-        tokenizedCode.push(CodeTokens.FOR);
-        // While line
-      } else if (
-        trimedLine.startsWith(languageIdentifier.whileIdentifier()) &&
-        trimedLine.endsWith(languageIdentifier.endLoopAndCondIdentifier())
-      ) {
-        tokenizedCode.push(CodeTokens.WHILE);
-        // If line
-      } else if (
-        trimedLine.startsWith(languageIdentifier.ifIdentifier()) &&
-        trimedLine.endsWith(languageIdentifier.endLoopAndCondIdentifier())
-      ) {
-        tokenizedCode.push(CodeTokens.IF);
-        const andOccurrences = CodeTokenizer.numberTokenPresentInLine(
-          languageIdentifier.andIdentifier(),
-          trimedLine,
-        );
-        tokenizedCode = CodeTokenizer.pushTokensInTab(
-          tokenizedCode,
-          CodeTokens.AND,
-          andOccurrences,
-        );
+          // For line
+        } else if (
+          trimedLine.startsWith(languageIdentifier.forIdentifier()) &&
+          trimedLine.endsWith(languageIdentifier.endLoopAndCondIdentifier())
+        ) {
+          tokenizedCode.push(CodeTokens.FOR);
+          // While line
+        } else if (
+          trimedLine.startsWith(languageIdentifier.whileIdentifier()) &&
+          trimedLine.endsWith(languageIdentifier.endLoopAndCondIdentifier())
+        ) {
+          tokenizedCode.push(CodeTokens.WHILE);
+          // If line
+        } else if (
+          trimedLine.startsWith(languageIdentifier.ifIdentifier()) &&
+          trimedLine.endsWith(languageIdentifier.endLoopAndCondIdentifier())
+        ) {
+          tokenizedCode.push(CodeTokens.IF);
+          const andOccurrences = CodeTokenizer.numberTokenPresentInLine(
+            languageIdentifier.andIdentifier(),
+            trimedLine,
+          );
+          tokenizedCode = CodeTokenizer.pushTokensInTab(
+            tokenizedCode,
+            CodeTokens.AND,
+            andOccurrences,
+          );
 
-        const orOccurrences = CodeTokenizer.numberTokenPresentInLine(
-          languageIdentifier.orIdentifier(),
-          trimedLine,
-        );
-        tokenizedCode = CodeTokenizer.pushTokensInTab(
-          tokenizedCode,
-          CodeTokens.OR,
-          orOccurrences,
-        );
+          const orOccurrences = CodeTokenizer.numberTokenPresentInLine(
+            languageIdentifier.orIdentifier(),
+            trimedLine,
+          );
+          tokenizedCode = CodeTokenizer.pushTokensInTab(
+            tokenizedCode,
+            CodeTokens.OR,
+            orOccurrences,
+          );
 
-        // Case line
-      } else if (
-        trimedLine.startsWith(languageIdentifier.caseIdentifier()) &&
-        trimedLine.endsWith(languageIdentifier.endLoopAndCondIdentifier())
-      ) {
-        tokenizedCode.push(CodeTokens.CASE);
-        // Other lines
-      } else {
-        tokenizedCode.push(CodeTokens.LINE);
-      }
+          // Case line
+        } else if (
+          trimedLine.startsWith(languageIdentifier.caseIdentifier()) &&
+          trimedLine.endsWith(':')
+        ) {
+          tokenizedCode.push(CodeTokens.CASE);
+          // Other lines
+        } else {
+          tokenizedCode.push(CodeTokens.LINE);
+        }
+
+      console.log(trimedLine);
     });
 
     console.log(tokenizedCode);
@@ -95,7 +103,8 @@ export class CodeTokenizer {
 
   private static numberOfFunctionParameters(line: string): number {
     const attributes = line.substring(line.indexOf('('), line.indexOf(')'));
-    return this.numberTokenPresentInLine(',', attributes) + 1;
+    const attribNb = this.numberTokenPresentInLine(',', attributes);
+    return attribNb === 0 ? 0 : attribNb + 1;
   }
 
   private static numberTokenPresentInLine(token: string, line: string): number {
---
+++ src/code-quality/tokenizer/identifiers/GolangIdentifiers.ts
@@ -0,0 +1,7 @@
+import { CommonLangIdentifiers } from './CommonLangIdentifiers';
+
+export class GolangIdentifiers extends CommonLangIdentifiers {
+  functionIdentifier(): string {
+    return 'func';
+  }
+}
